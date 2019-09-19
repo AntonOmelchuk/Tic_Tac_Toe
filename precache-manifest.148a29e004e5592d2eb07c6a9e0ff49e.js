194955ab0058d6ae04da79d3fb15019f28945e2e
Anton <kino_kg@ukr.net>
2019-09-19 20:33:21 +0300

    Updates

---
+++ precache-manifest.148a29e004e5592d2eb07c6a9e0ff49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7677f51b370da2c1f0425ffb1dc7de3",
+    "url": "/Tic_Tac_Toe/index.html"
+  },
+  {
+    "revision": "4bbe5632d5dcd8fbcf58",
+    "url": "/Tic_Tac_Toe/static/css/main.65627c4b.chunk.css"
+  },
+  {
+    "revision": "3a0fa95e4edce5fce805",
+    "url": "/Tic_Tac_Toe/static/js/2.0e3c079a.chunk.js"
+  },
+  {
+    "revision": "4bbe5632d5dcd8fbcf58",
+    "url": "/Tic_Tac_Toe/static/js/main.7969ca38.chunk.js"
+  },
+  {
+    "revision": "33c93ca2286d33cbbe1e",
+    "url": "/Tic_Tac_Toe/static/js/runtime~main.8f4f1e22.js"
+  },
+  {
+    "revision": "66bb8429381236196b3f752aa1840938",
+    "url": "/Tic_Tac_Toe/static/media/logo.66bb8429.png"
+  }
+]);
